614c04cbe80fb7222f3b30011c4ee875d12c363e
David8633 <davidinstituto282@gmail.com>
2025-02-06 10:53:38 +0100

    añadido los mapas

--- Tests-David/src/entorno_test/tets.java
+++ Tests-David/src/entorno_test/tets.java
@@ -1,8 +1,14 @@
-package actividades;
+package entorno_test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
+import actividades.A4_1;
+
 import static actividades.A4_1.contarPalabras;
 import static actividades.A4_1. contarLetrasDiferentes;
 import static actividades.A4_1.contarFrecuenciaLetras;
@@ -24,11 +30,30 @@ class tets {
 		assertEquals(2,contarLetrasDiferentes("ae"));
 		assertEquals(0,contarLetrasDiferentes(null));
 	}
-	/*
+	
 	@Test
-	void testContarFrecunciaLetras() {
+	void testContarFrecunciaPalabra() {
+		Map<String,Integer> esperada1 = new HashMap<>(Map.of("mundo",2));
+		/*Hemos añadido en el codigo A4_1 el metodo trim para que no nos de fallo*/
+	
+		assertTrue(esperada1.equals(A4_1.contarFrecuenciaPalabras("mundo mundo")));
+		assertFalse(esperada1.equals(A4_1.contarFrecuenciaPalabras("mundo")));
+		assertFalse(esperada1.equals(A4_1.contarFrecuenciaPalabras("")));
+		
 	}
-	*/
+	
+	@Test
+	void testContarFrecunciaLetra() {
+		Map<Character,Integer> esperada2 = new HashMap<>(Map.of('m',2));
+	
+		assertTrue(esperada2.equals(A4_1.contarFrecuenciaLetras("m m")));
+		assertFalse(esperada2.equals(A4_1.contarFrecuenciaLetras("mundo")));
+		assertFalse(esperada2.equals(A4_1.contarFrecuenciaLetras("")));
+		
+	}
+	
+
+	
 	
 	@Test 
 	void testEsPalindroma() {
@@ -38,5 +63,6 @@ class tets {
 		assertEquals(true,esPalindroma(" ANA"));
 		assertEquals(false,A4_1.esPalindroma("aNi"));
 		assertEquals(true,esPalindroma(" ANA " ));
+		assertEquals(true,esPalindroma(" A1A " ));
 	}
 }
